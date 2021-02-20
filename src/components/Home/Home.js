048a29c1901ecfa9378e7bf74497dfb142908411
Raymond Youssef <rimonomega@gmail.com>
2021-02-20 15:30:26 +0200

    added intro componenet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.scss';
+import Intro from "./Intro/Intro";
 
 export default function Home() {
-    return(
-        <div id={'home'}>
 
+    return(
+        <div id={'home'} >
+            <Intro />
         </div>
     )
 }
