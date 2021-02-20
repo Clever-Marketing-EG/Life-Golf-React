5f2a86fb5959c6cc2878c0b6834b6ddc8518bb15
Ahmed Salem <malekdekwin@gmail.com>
2021-02-20 16:08:21 +0200

    products

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
 import Footer from "./components/Shared/Footer/Footer";
 
-
 function App() {
-
-    return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/Products">
+            <Products />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
