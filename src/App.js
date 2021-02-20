e8416e7701ffb2240110398fb6f4b4131b75ff69
Rana Medhat <rana.medhat69@gmail.com>
2021-02-20 17:05:39 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 import Home from "./components/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import ContactUs from "./components/ContactUs/ContactUs"
-
+import AboutUs from './components/AboutUs/AboutUs';
 function App() {
 
     return (
@@ -16,6 +16,9 @@ function App() {
                 <Switch>
                     <Route exact path="/">
                         <Home />
+                    </Route>
+                    <Route exact path="/About">
+                        <AboutUs />
                     </Route>
                     <Route exact path="/Contact">
                         <ContactUs />
