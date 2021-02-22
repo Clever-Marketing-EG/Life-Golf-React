ae8e22a6cc33d847d99f4e025a3b7cfeca93eb94
Raymond Youssef <rimonomega@gmail.com>
2021-02-22 18:19:13 +0200

    fix

--- src/components/Home/OurNews/OurNews.js
+++ src/components/Home/OurNews/OurNews.js
@@ -9,6 +9,53 @@ import ArticleImg3 from '../Assets/about-us-3.jpg'
 
 export default function OurNews() {
 
+    const data = [
+        {
+            title: 'Article One',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg1,
+            date: '19 Jan'
+        },
+        {
+            title: 'Article Two',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg2,
+            date: '28 Feb'
+        },
+        {
+            title: 'Article Three',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg3,
+            date: '11 Mar'
+        },
+        {
+            title: 'Article Four',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg1,
+            date: '22 Apr'
+        },
+        {
+            title: 'Article Five',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg2,
+            date: '15 May'
+        },
+        {
+            title: 'Article Six',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg3,
+            date: '13 Jun'
+        },
+        {
+            title: 'Article Seven',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia porta eros in mollis. Sed felis ligula, cursus eget finibus eu, hendrerit eget mauris.',
+            image_url: ArticleImg1,
+            date: '5 Jul'
+        },
+    ]
+
+
+
     const breakPoints = [
         { width: 1, itemsToShow: 1 },
         { width: 550, itemsToShow: 2 },
@@ -48,16 +95,16 @@ export default function OurNews() {
                     renderArrow={myArrow}
                     breakPoints={breakPoints}
                 >
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
+                    {
+                        data.map(article => (
+                            <Article
+                                title={article.title}
+                                content={article.content}
+                                image_url={article.image_url}
+                                date={article.date}
+                            />
+                        ))
+                    }
                 </Carousel>
             </div>
 
@@ -66,17 +113,16 @@ export default function OurNews() {
 }
 
 
-function Article() {
+function Article({ title, content, date, image_url}) {
     return(
         <div className="article card" style={{width: '18rem'}}>
-            <img src={ArticleImg1} className="card-img-top" alt="..." />
+            <img src={image_url} className="card-img-top" alt="..." />
             <div className={'date'}>
-                19 Jan
+                {date}
             </div>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk
-                    of the card's content.</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{content}</p>
                 <a href="#" className="cherry-link">Read More <i className="icon-arrow-right"/></a>
             </div>
         </div>
