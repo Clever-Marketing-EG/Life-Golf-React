2760fee8f460ac6ef846986c9b6fe77aa749df4c
Raymond Youssef <rimonomega@gmail.com>
2021-02-22 11:32:55 +0200

    our services initialization

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import './Home.scss';
 import Intro from "./Intro/Intro";
 import AboutUs from "./AboutUs/AboutUs";
 import OurProducts from "./OurProducts/OurProducts";
+import OurServices from "./OurServices/OurServices";
 
 export default function Home() {
 
@@ -11,6 +12,7 @@ export default function Home() {
             <Intro />
             <AboutUs />
             <OurProducts />
+            <OurServices />
         </div>
     )
 }
