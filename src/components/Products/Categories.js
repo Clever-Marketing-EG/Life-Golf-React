746381fd90921e382f14a6b85361ab8988a26af3
Raymond Youssef <rimonomega@gmail.com>
2021-03-27 18:26:57 +0200

    fixed Categories component

--- src/components/Products/Categories.js
+++ src/components/Products/Categories.js
@@ -3,28 +3,32 @@ import Header from "../Shared/Header/Header";
 import {Link} from 'react-router-dom'
 import './Products.scss'
 export default function Categories({categories , changelang}) {
+
+    const setCategory = (item) => {
+        localStorage.setItem("category_id",item.id)
+    }
+
     return (
         <div id="products">
-        <Header changelang="changelang" title={'Categories'} />
-        <br />
-        <div className="container">
-        <div className="row">
-            {
-                categories.map(item => {
-                    return(
-                        <div className="col-3">
-                            <div className="category_item">
-                            <img className="cat-img" src={item.image_url} /><br />
-                            <Link onClick={()=>{
-                                localStorage.setItem("category_id",item.id)
-                            }} to="/Products"><p className="cat-title">{item.name}</p></Link>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-        </div>
+            <Header changelang={changelang} title={'Categories'} />
+            <br />
+            <div className="container">
+                <div className="row">
+                    {
+                        categories.map( (item, index) => {
+                            return(
+                                <div className="col-3" key={index}>
+                                    <div className="category_item">
+                                        <img className="cat-img" src={item.image_url}  alt={'...'}/>
+                                        <br />
+                                        <Link onClick={setCategory(item)} to="/Products"><p className="cat-title">{item.name}</p></Link>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
