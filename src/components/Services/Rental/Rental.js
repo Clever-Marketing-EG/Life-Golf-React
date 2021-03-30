73e1addf9485e5a8620d75331cf2c92f7d34e4e7
Rana Medhat <rana.medhat69@gmail.com>
2021-03-30 16:38:31 +0200

    fixes

--- src/components/Services/Rental/Rental.js
+++ src/components/Services/Rental/Rental.js
@@ -38,8 +38,9 @@ export default function Rental({ changelang }) {
 
         })
     }, [localStorage.getItem('lang')])
-   
-
+    var test =  service.points;
+    console.log(test);
+ 
     return (
         <div id={'rental'}>
             <Header changelang={changelang} className="header" title={service.name} />
