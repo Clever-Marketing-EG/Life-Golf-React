df63fa616dbbf58c73fbe0c13357c044294edf12
Raymond Youssef <rimonomega@gmail.com>
2021-04-01 19:02:30 +0200

    fixed products page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,80 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import "./Products.scss";
-import product from "../../assets/Products/product.png";
-import icon1 from "../../assets/Products/icon-1.svg";
-import icon2 from "../../assets/Products/icon-2.svg";
-import icon3 from "../../assets/Products/icon-3.svg";
 import Header from "../Shared/Header/Header";
 import {Link} from 'react-router-dom'
 import axios from 'axios'
+
+
+const { BASE_URL } = require('../../config');
+
+
 export default function Products() {
-  const { BASE_URL } = require('../../config');
-  const [products, setProducts] = useState([]);
-  const [cat, setCat] = useState([]);
-  useEffect(() => {
-    let catid = localStorage.getItem("category_id")
-    axios.get(`${BASE_URL}/categories/${catid}`).then(response => {
-      setCat(response.data.data);
-    });
-    axios.get(`${BASE_URL}/categories/${catid}/products`).then(response => {
-      setProducts(response.data.data);
-    });
-  },[])
-  return (
-      <div id={"products"}>
-        <Header title={cat.name} />
-        <center>
-        <div className="container">
-        <div className="row">
-       {products.map(item=>{
-         return (
-          <div className="col ">
-          <div className="card col-card">
-            <div className="head-div">
-              {" "}
-              <p className="tag">2016</p>
-              <img src={item.image_url} className="card-img-top product-img" alt="..." />
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/products`).then(response => {
+            console.log(response.data.data);
+            const lang = localStorage.getItem('lang');
+            let dataArr;
+            if (lang === 'ar') {
+                dataArr = response.data.data.map( item => ({
+                    id: item.id,
+                    name: item.name_ar,
+                    features: item.features_ar,
+                    year: new Date(item.created_at).getFullYear(),
+                    image_url: item.images[0] ? item.images[0].url : ''
+                }))
+            } else {
+                dataArr = response.data.data.map( item => ({
+                    id: item.id,
+                    name: item.name,
+                    features: item.features,
+                    year: new Date(item.created_at).getFullYear(),
+                    image_url: item.images[0] ? item.images[0].url : ''
+                }))
+            }
+            console.log(dataArr);
+            setProducts(dataArr);
+        });
+    },[])
+
+
+    return (
+        <div>
+            <Header title={'Products'} />
+            <div>
+                <div id={"products"} className="container">
+                    <div className="d-flex flex-wrap justify-content-around align-items-center">
+                        {
+                            products.map( (item, index) => (
+                                <ProductCard item={item} key={index} />
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
-            <div className="card-body">
-              <h5 className="title">{item.name}</h5>
-              <br />
-              <br />
-              <div className="mrow">
-                <div className="col-4">
-                  <div className="disc">
-                    <img className="icon_d" src={icon1}  alt={'...'}/>
-                    <p>20KM/h</p>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="disc">
-                    <img className="icon_d"  src={icon2} />
-                    <p>20KM/h</p>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="disc">
-                    <img  className="icon_d"  src={icon3} />
-                    <p>20KM/h</p>
-                  </div>
-                </div>
-                <br />
-                <br />
-                <br />
-                <Link onClick={()=>{
-                  localStorage.setItem("product_id",item.id)
-                }} to="/Product">
-                <button className="btn primarybtn">See More</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>  
-         )
-       })}
-          </div>
         </div>
-        </center>
-      </div>
-  );
+    );
+}
+
+
+function ProductCard( {item} ) {
+    return (
+        <div className="col">
+            <div className="card col-card">
+                <div className="head-div position-relative">
+                    <p className="tag position-absolute top-10 end-0 bg-white">{item.year}</p>
+                    <img src={item.image_url} className="card-img-top product-img" alt="..." />
+                </div>
+                <div className="card-body">
+                    <h5 className="title">{item.name}</h5>
+                    <p>
+                        {item.features}
+                    </p>
+                    <br />
+                    <br />
+                    <div className="mrow">
+                        <Link to={`/products/${item.id}`} className={'btn primarybtn block w-100'}>
+                            See More
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
